294a9065ca818d208b2e3ca8e6f1ef083e71059c
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-11-23 14:11:27 +0000

    rake db:seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dir.mktmpdir do |dir|
+  system "unzip ./db/data.zip -d #{dir}"
+
+  ActiveRecord::Base.connection.execute 'truncate routes;'
+  ActiveRecord::Base.connection.execute 'truncate stops;'
+
+  puts 'Parsing routes'
+
+  File.open("#{dir}/routes.txt", "r").each_line do |line|
+    if line.start_with? 'route_id' then next end
+
+    Route.import(line).save!
+  end
+
+  puts "#{Route.all.count} routes imported"
+
+
+  puts 'Parsing stops'
+
+  File.open("#{dir}/stops.txt", "r").each_line do |line|
+    if line.start_with? 'stop_id' then next end
+
+    Stop.import(line).save!
+  end
+
+  puts "#{Stop.all.count} stops imported"
+
+end
