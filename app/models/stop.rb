41494c33754e67288a5faa6c81980300466543ab
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-11-23 11:22:58 +0000

    Stop parse from raw data

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,13 @@
 class Stop < ActiveRecord::Base
+
+  def self.import line
+    stop = Stop.new
+
+    parts = line.split ','
+    stop.code = parts[0]
+    stop.name = parts[2][1, parts[2].length-2].split.map(&:capitalize).join ' '
+
+    stop
+  end
+
 end
--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Stop, :type => :model do
 
+  describe 'import' do
+
+    it 'should parse a line from the raw data' do
+      stop = Stop.import '1FMBL,11327,"FOCH MAISON BLEUE",,2278373.0,382488.0,,,0,place_CENTRE'
+
+      expect(stop.name).to eq('Foch Maison Bleue')
+      expect(stop.code).to eq('1FMBL')
+    end
+
+  end
+
 end
