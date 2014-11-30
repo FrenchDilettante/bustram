ba5bb56f222dadbb3586737e821933d53afce010
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-11-30 22:28:49 +0000

    Sample capistrano config file

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 
+gem 'capistrano', '3.2.1'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'coveralls', require: false
 gem 'friendly_id', '5.0.4'
@@ -12,6 +13,11 @@ gem 'rails', '4.1.7'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'turbolinks'
+
+# Capistrano gems
+gem 'capistrano-bundler', '1.1.3'
+gem 'capistrano-rvm', '0.1.2'
+gem 'capistrano-rails', '1.1.2'
 
 group :development do
   gem 'spring'
---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'capistrano/bundler'
+
+lock '3.2.1'
+
+set :application, 'bustram'
+set :repo_url, 'git@github.com:manudwarf/bustram.git'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+end
