6c29c76611e26c1d8ae88f6ab3f6f024139a722b
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-12-08 07:43:53 +0000

    Display estimated arrival time

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -3,6 +3,7 @@ module = angular.module('bustram.stops', []);
 module.controller('StopController',
   ['$scope', '$http',
   function ($scope, $http) {
+
     function getCurrentStopId() {
       return location.pathname.split('/')[2];
     }
@@ -18,5 +19,24 @@ module.controller('StopController',
         });
     }
 
+    $scope.estimatedTime = function (time) {
+      return Math.trunc((time - new Date()) / 60000);
+    };
+
+    $scope.toggleDisplay = function () {
+      $scope.showDeparture = !$scope.showDeparture;
+    }
+
+    $scope.showDeparture = false;
+
     loadSchedules();
   }]);
+
+module.filter('remaining',
+  function () {
+    return function (remaining) {
+      var hours = Math.trunc(remaining / 60),
+          mins = remaining % 60;
+      return (hours > 0 ? hours + 'h' : 0) + mins + 'm';
+    }
+  });
