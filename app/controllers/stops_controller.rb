ebcb8b60d877a73a397830a13c7e1bcb1c5fca7a
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-12-09 20:12:55 +0000

    Code coverage

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,7 +2,6 @@ class StopsController < ApplicationController
 
   def show
     @stop = Stop.friendly.find params[:id]
-    @schedules = Schedule.next @stop
   end
 
 end
--- spec/controllers/stops_controller_spec.rb
+++ spec/controllers/stops_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe StopsController, :type => :controller do
 
+  describe 'show' do
+    stop = nil
+    before(:each) do
+      stop = Stop.create name: 'test'
+      get :show, id: stop.slug
+    end
+
+    it 'should render the stop' do
+      expect(response.code).to eq('200')
+      expect(assigns(:stop)).to eq(stop)
+    end
+  end
+
 end
