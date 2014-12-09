1fe033716ac596412d4bdc023434030d4665e8e2
Emmanuel Bourgerie <manu@bourgerie.fr>
2014-12-09 20:02:30 +0000

    Code coverage

--- spec/api/stops_api_spec.rb
+++ spec/api/stops_api_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe V1::StopsApi do
+  stop_test = nil
 
   before(:each) do
-    Stop.create name: 'Stop Test'
+    stop_test = Stop.create name: 'Stop Test'
     Stop.create name: 'Another Stop'
   end
 
@@ -49,14 +50,27 @@ describe V1::StopsApi do
 
   describe 'GET /api/v1/stops/:id/schedules' do
 
+    before(:each) do
+      timeframe = Timeframe.create start_date: Time.now - 1.days, end_date: Time.now + 1.days, monday: true, tuesday: true, wednesday: true, thursday: true, friday: true, saturday: true, sunday: true
+      route = Route.create short_name: 'T'
+      trip = Trip.create timeframe: timeframe, route: route, headsign: 'headsign'
+      next_time = (Time.now + 10.minutes).strftime('%H%M%S').to_i
+      Schedule.create stop: stop_test, trip: trip, departure_time: next_time
+    end
+
     it 'should get the next schedules' do
       get '/api/v1/stops/stop-test/schedules'
 
       expect(response.status).to be(200)
-      # @TODO cannot be tested without complete data set
-      # parsed = JSON.parse response.body
-      # expect(parsed.length).to be(10)
-      # expect(parsed[0].keys).to eq(['departure_time', 'route_id', 'headsign'])
+      parsed = JSON.parse response.body
+      expect(parsed.length).to be(1)
+      schedule = parsed[0]
+      expect(schedule.keys).to eq(['departure_time', 'route_id', 'headsign'])
+      expect(schedule['headsign']).to eq('headsign')
+      expect(schedule['route_id']).to eq('T')
+      expect(schedule['departure_time']).to_not be_nil
+      expect { Time.parse schedule['departure_time'] }.not_to raise_error
+      
     end
 
   end
