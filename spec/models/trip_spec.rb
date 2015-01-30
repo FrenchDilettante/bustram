4d9eb8358a4bb7162a4acc5e2eb657254548b8de
Emmanuel Bourgerie <emmanuel.bourgerie@teamaol.com>
2015-01-30 13:27:43 +0000

    Test fixed

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Trip, :type => :model do
-  correct_route = nil
-  correct_timeframe = nil
-
-  before(:each) do
-    correct_route = Route.import '"1d-131,1d,""DJF  Val de Maine > Eventard"",,3,"'
-    correct_timeframe = Timeframe.import '2015H-DJF_01-Dimanche-71,0,0,0,0,0,0,1,20141109,20141207'
-  end
 
   describe 'import' do
 
     it 'should parse a trip from the raw data' do
-      trip = Trip.import '1d-131,2015H-DJF_01-Dimanche-71,2378970-2015H-DJF_01-Dimanche-71,"VAL de MAINE",1,606529,1d0045'
+      route_ids = {'1d-131' => 111}
+      timeframe_ids = {'2015H-DJF_01-Dimanche-71' => 222}
+      trip = Trip.import '1d-131,2015H-DJF_01-Dimanche-71,2378970-2015H-DJF_01-Dimanche-71,"VAL de MAINE",1,606529,1d0045', route_ids, timeframe_ids
 
-      expect(trip.route_id).to be(correct_route.id)
-      expect(trip.timeframe_id).to be(correct_timeframe.id)
+      expect(trip.route_id).to be(111)
+      expect(trip.timeframe_id).to be(222)
       expect(trip.code).to eq('2378970-2015H-DJF_01-Dimanche-71')
       expect(trip.headsign).to eq('VAL de MAINE')
       expect(trip.direction_id).to be(1)
