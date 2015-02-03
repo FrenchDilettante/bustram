d792121334a9bdd213fff6ab8642f71f856bc38e
Emmanuel Bourgerie <manu@bourgerie.fr>
2015-02-03 19:46:58 +0000

    Fixed failing test

--- spec/api/stops_api_spec.rb
+++ spec/api/stops_api_spec.rb
@@ -37,7 +37,7 @@ describe V1::StopsApi do
       expect(response.status).to be(200)
       parsed = JSON.parse response.body
       expect(parsed['name']).to eq('Stop Test')
-      expect(parsed.keys).to eq(['id', 'name', 'locations', 'routes'])
+      expect(parsed.keys).to eq(['id', 'name', 'routes', 'locations'])
     end
 
     it 'should throw a 404 if not found' do
